18d555ca28af9bc17044d70221c5c2cf92a49cd2
sunshineinsandiego <chris@chris.com>
2023-09-04 12:13:10 -0700

    fei

--- annotate_video.py
+++ annotate_video.py
@@ -4,27 +4,25 @@
  
 import numpy as np
 import cv2
-import csv
 
-# load main dataframe
+localization_file = open('FILE.txt', 'w')
 
-# function to display the coordinates of
-# of the points clicked on the image
 def mouse_event(event, x, y, flags, params):
     
-    frame_counter = int(params[0])
+    frame_count = params[0]
+    f = params[1]
 
     # checking for mouse clicks
     if (event == cv2.EVENT_MOUSEMOVE) or (event == cv2.EVENT_LBUTTONDOWN) or (event == cv2.EVENT_RBUTTONDOWN):
         
-        text_to_save = [frame_counter, x, y]
+        text_to_save = str(frame_count) + ',' + str(x) + ',' + str(y) + '\n'
         print(text_to_save)
-        with open("keypoint_labels.csv", "a", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow(text_to_save)
+        f.write(text_to_save)
+        
+        return True
 
 # Read dvideo and pvideo images
-video_cap = cv2.VideoCapture('../journal_dataset/right_video.mp4')
+video_cap = cv2.VideoCapture('VIDEO_FILE.mp4')
 total_video_frames = video_cap.get(7)
 
 for i in range(1, int(total_video_frames + 1)):
@@ -35,8 +33,7 @@ for i in range(1, int(total_video_frames + 1)):
     video_ret, video_frame = video_cap.read()
     # Display each dvideo_frame
     cv2.imshow('video_frame', video_frame)
-    print(i)
     # set mouse callback
-    cv2.setMouseCallback('video_frame', mouse_event, param = [i])
+    cv2.setMouseCallback('video_frame', mouse_event, param = [i, localization_file])
     # show one frame at a time
-    cv2.waitKey(0)
+    cv2.waitKey(1)
